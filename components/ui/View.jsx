37f4c80ca37643ebc02741f26325909c11fca754
Ceo-Sammarco <noah.kd@gmail.com>
2026-05-09 19:36:57 -0300

    update:  M components/ui/View.jsx

--- components/ui/View.jsx
+++ components/ui/View.jsx
@@ -1,8 +1,18 @@
+/** 
+  * --------- */ 
+const VALID_TAGS = new Set([
+  "div", "section", "main", "article", "aside",
+  "header", "footer", "nav", "span", "ul", "ol", "li", "p",
+]);
+
 /**
  * --- NK-View ---
- * @param {string} tag - A tag container (div, section, main, etc)
+ * @param {string} [tag="div"] - Tag HTML do container
+ * @param {string} [className] - Classes CSS adicionais
+ * @param {React.ReactNode} children
  */
 export default function View({ children, tag = "div", ...props }) {
   const Tag = tag;
+  // const Tag = VALID_TAGS.has(tag) ? tag : "div";
   return <Tag {...props}>{children}</Tag>;
 }
